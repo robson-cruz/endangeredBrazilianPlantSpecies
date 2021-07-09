a8f766ac2cfe96cb596ad6536d8c2b4d6e90c6b9
Robson Cruz <rcflorestal@yahoo.com.br>
2021-07-09 13:47:59 -0300

    app server

---
+++ app/server.R
@@ -0,0 +1,32 @@
+library(shiny)
+
+source('./scripApp.R')
+
+sp <- read.csv('./endangered_BRA.csv', sep = ';', encoding = 'latin1')
+
+shinyServer(function(input, output, session) {
+        # Data frame
+        output$tbl <- DT::renderDataTable({
+                DT::datatable(sp, 
+                              options = list(pageLength = 6, rownames = FALSE))
+        })
+        # Charts
+        output$bra <- renderPlot(bra, width = 800, height = 450)
+        output$status <- renderPlot(sp_status, width = 800, height = 450)
+        output$family <- renderPlot(f, width = 800, height = 450)
+        
+        #Download
+        output$DownloadData <- downloadHandler(
+                filename = function() {
+                        paste('speciesProtegidas', Sys.Date(), '.csv', sep = '_')
+                },
+                content = function(file) {
+                        write.csv2(sp, file)
+                }
+        )
+        
+        session$onSessionEnded(function() {
+                stopApp()
+        })
+})
+
