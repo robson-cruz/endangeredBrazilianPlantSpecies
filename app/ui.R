26801872e88b75ea22230b83bf792ba0aa47203f
Robson Cruz <rcflorestal@yahoo.com.br>
2021-09-20 23:38:49 -0300

    ui app

--- app/ui.R
+++ app/ui.R
@@ -6,15 +6,12 @@ Sys.setlocale(category = 'LC_ALL', locale = 'Portuguese')
 shinyUI(navbarPage(
         title = 'STATUS FLORA BRASIL v1.2.0',
         h3('FLORA BRASILEIRA AMEAÇADA DE EXTINÇÃO'),
-        br(),
         tabPanel('Lista', DT::dataTableOutput('tbl')),
         tabPanel('Lista Nacional', plotOutput('bra')),
         tabPanel('Categorias', plotOutput('status')),
         tabPanel('Familias', plotOutput('family')),
         tabPanel('Download', downloadLink('DownloadData', 'Download')),
-        br(),
-        br()
+        tabPanel(a(href='https://github.com/rcflorestal/endangeredBrazilianPlantSpecies/blob/main/README.md', 'Leia!'))
         #h5('Santos, Robson Cruz. Espécies da Flora Brasileira Ameaçadas de Extinção. Disponível em: https://wcj7es-robson0cruz.shinyapps.io/specBra/ Acesso em: Jul 2021. DOI: https://zenodo.org/record/5083815')
         )
-        
 )
