4c569092333bcd77d2bfb8fc7013ff3a12e15f7a
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2022-08-02 15:48:39 -0300

    Webscraping

--- WebScraping_Port_MMA443.R
+++ WebScraping_Port_MMA443.R
@@ -2,7 +2,7 @@
 #
 library(rvest)
 
-# Tabela do Brasileirao Serie A
+# Tabela Portaria MMA 443
 html_tab <- 'https://www.in.gov.br/en/web/dou/-/portaria-mma-n-561-de-15-de-dezembro-de-2021-367747322'
 
 tab <- html_tab %>%
