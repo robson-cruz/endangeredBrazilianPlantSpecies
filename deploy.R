f55e36148017ac5438706f84f133b978fba638ca
Robson Cruz <rcflorestal@yahoo.com.br>
2023-03-15 20:04:21 -0300

    script to deploy

---
+++ deploy.R
@@ -0,0 +1,5 @@
+rsconnect::deployApp('D:/data/endangeredBrazilianPlantSpecies/app/',
+                     appTitle = 'specBra', appName = 'specBra',
+                     account = 'rcdev',
+                     forceUpdate = TRUE,
+                     appId = 4367290)
