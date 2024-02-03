6d4991150cbf52a902fd8cd959abecd637649c7b
Robson Cruz <rcflorestal@yahoo.com.br>
2024-02-03 10:45:08 -0300

    Navbar organized, add Theme

--- app/ui.R
+++ app/ui.R
@@ -1,18 +1,34 @@
-## specBra 1.2.0
 library(shiny)
 
-Sys.setlocale(category = 'LC_ALL', locale = 'Portuguese')
 
-shinyUI(navbarPage(
-        title = 'STATUS FLORA BRASIL v1.2.0',
-        h3('FLORA BRASILEIRA AMEAÇADA DE EXTINÇÃO'),
-        tabPanel('Lista', DT::dataTableOutput('tbl')),
-        tabPanel('Lista Nacional', plotOutput('bra')),
-        tabPanel('Categorias', plotOutput('status')),
-        tabPanel('Familias', plotOutput('family')),
-        tabPanel('Download', downloadLink('DownloadData', 'Download')),
-        tabPanel(a(href = 'https://github.com/rcflorestal/endangeredBrazilianPlantSpecies/blob/main/README.md', 'Leia!'))
-        #h5('Santos, Robson Cruz. Espécies da Flora Brasileira Ameaçadas de Extinção. Disponível em: https://wcj7es-robson0cruz.shinyapps.io/specBra/ Acesso em: Jul 2021. DOI: https://zenodo.org/record/5083815')
+shinyUI(
+        fluidPage(
+                theme = shinythemes::shinytheme('flatly'),
+                navbarPage(
+                        tags$head(includeHTML('google-analytics.html')),
+                        title = 'STATUS FLORA BRASIL v1.2.0',
+                        lang = "pt-BR",
+                        h3('FLORA BRASILEIRA AMEAÇADA DE EXTINÇÃO'),
+                        tabPanel('Lista', DT::dataTableOutput('tbl')),
+                        tabPanel('Lista Nacional', plotOutput('bra')),
+                        tabPanel('Categorias', plotOutput('status')),
+                        tabPanel('Familias', plotOutput('family')),
+                        navbarMenu(
+                                'Download',
+                                tabPanel(
+                                        downloadButton(
+                                                outputId = 'DownloadData',
+                                                label = 'Lista de Espécies Ameaçadas'
+                                        )
+                                )
+                        ),
+                        navbarMenu(
+                                "Metodologia",
+                                tabPanel(
+                                        HTML("<a href = 'https://github.com/rcflorestal/endangeredBrazilianPlantSpecies/blob/main/README.md' target = '_blank'>Leia!</a>")
+                                )
+                        )
+                        #h5('Santos, Robson Cruz. Espécies da Flora Brasileira Ameaçadas de Extinção. Disponível em: https://wcj7es-robson0cruz.shinyapps.io/specBra/ Acesso em: Jul 2021. DOI: https://zenodo.org/record/5083815')
+                )
         )
 )
-
