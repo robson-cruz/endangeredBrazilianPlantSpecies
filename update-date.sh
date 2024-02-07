223b5a5b301b7cee00d724f13276cc3747c2a352
Robson Cruz <rcflorestal@yahoo.com.br>
2024-02-07 08:26:04 -0300

    Update update-date.sh

--- update-date.sh
+++ update-date.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 # Define a data atual
-CURRENT_DATE=$(date "+%B %Y")
+CURRENT_DATE=$(date "+%B %Y" --quiet)
+
+if [[ $? -ne 0 ]]; then
+  echo "Erro ao obter a data atual!" >&2
+  echo "Verifique se o comando 'date' está funcionando corretamente."
+  exit 1
+fi
 
 # Define o comando sed para substituir a data no README.md
 SED_COMMAND="s/Acesso em: [A-Za-z]* [0-9]\{4\}/Acesso em: ${CURRENT_DATE}/g"
 
 # Executa o comando sed para substituir a data no README.md
 sed -i -E "${SED_COMMAND}" README.md
+
+if [[ $? -ne 0 ]]; then
+  echo "Erro ao atualizar a data no README.md!" >&2
+  echo "Verifique se o comando 'sed' está instalado e se o arquivo README.md existe."
+  exit 1
+fi
+
+echo "Data atualizada no README.md com sucesso!"
 
 # Configura o nome e o email do usuário do Git
 git config user.email rcflorestal@yahoo.com.br
