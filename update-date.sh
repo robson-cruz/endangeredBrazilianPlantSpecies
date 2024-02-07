b9c9e8a0b725aacd95f3f55d4cfa1e440a21f0c3
Robson Cruz <rcflorestal@yahoo.com.br>
2024-02-06 21:51:36 -0300

    Update update-date.sh

--- update-date.sh
+++ update-date.sh
@@ -5,9 +5,10 @@ SED_COMMAND="s/Acesso em: \w+ \d{4}/Acesso em: ${CURRENT_DATE}/g"
 
 sed -i -E "${SED_COMMAND}" README.md
 
+git pull
 git config user.email rcflorestal@yahoo.com.br
 git config user.name rcDeveloping
-git pull
+
 git add README.md
 git commit -m "Atualizar data no README"
 git push
