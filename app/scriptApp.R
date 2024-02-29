ca0c5ef708225e7de7e59fd1baf5e9af1cc9eef1
rcDeveloping <rcflorestal@yahoo.com.br>
2024-02-29 13:56:49 -0300

    update reflora data to version 393.397

--- app/scriptApp.R
+++ app/scriptApp.R
@@ -1,17 +1,20 @@
 library(dplyr, warn.conflicts = FALSE)
 library(ggplot2)
+library(glue)
+library(magrittr)
 
 
-# Read the data sets
-endangered_BRA <- read.csv2('./endangered_BRA.csv', sep = ';', encoding = 'latin1')
+# Read and join data sets
+endangered_BRA <- read.csv2('../output/Especies_Ameacadas_BRA.csv', fileEncoding = "latin1")
 
 # List Chart ##
 species_source <- endangered_BRA %>%
-        group_by(source) %>%
-        count(source, sort = TRUE)
-species_source$source <- with(species_source, reorder(source, n))
+        group_by(fonte) %>%
+        count(fonte, sort = TRUE)
 
-bra <- ggplot(species_source, aes(x = source, y = n, fill = n)) +
+species_source$fonte <- with(species_source, reorder(fonte, n))
+
+bra <- ggplot(species_source, aes(x = fonte, y = n, fill = n)) +
         geom_col() +
         coord_flip() +
         theme(plot.title = element_text(hjust = 0.5, size = 14),
@@ -25,19 +28,19 @@ bra <- ggplot(species_source, aes(x = source, y = n, fill = n)) +
 
 # Species Chart by status ##
 species_status <- endangered_BRA %>%
-        group_by(status) %>%
-        count(status, sort = T)
+        group_by(codStatus) %>%
+        count(codStatus, sort = T)
 
 # Reorder the data set by number of status 
-species_status$status <- with(species_status, reorder(status, desc(n)))
+species_status$codStatus <- with(species_status, reorder(codStatus, desc(n)))
 
 # Set status code table 
 statusCodeTable <- endangered_BRA %>%
-        group_by(status, statusSource) %>%
+        group_by(status_conservacao, codStatus) %>%
         summarise()
 
 # Set chart
-sp_status <- ggplot(species_status, aes(x = status, y = n)) +
+sp_status <- ggplot(species_status, aes(x = codStatus, y = n)) +
         geom_col(fill = 'steelblue') +
         scale_fill_manual(name = 'Status',
                           values = c('EN = Em Perigo')) +
@@ -55,14 +58,18 @@ sp_status <- ggplot(species_status, aes(x = status, y = n)) +
 
 # Family Chart ##
 statusFamily <- endangered_BRA %>%
-        filter(family != "") %>%
-        mutate(family = stringr::str_to_title(family)) %>%
-        count(family, sort = TRUE) %>%
-        slice_max(n, n=10)
+        filter(familia != "") %>%
+        mutate(familia = stringr::str_to_title(familia)) %>%
+        group_by(familia) %>%
+        summarise(n = n()) %>%
+        arrange(desc(n)) %>%
+        slice_head(n = 10)
 
-statusFamily$family <- with(statusFamily, reorder(family, n))
 
-f <- ggplot(statusFamily, aes(family, n)) +
+        
+statusFamily$familia <- with(statusFamily, reorder(familia, n))
+
+f <- ggplot(statusFamily, aes(familia, n)) +
         geom_col(fill = 'steelblue') + coord_flip() +
         geom_text(aes(label = n, hjust = 2), color = 'white') +
         theme(plot.title = element_text(hjust = 0.5, size = 14)) +
