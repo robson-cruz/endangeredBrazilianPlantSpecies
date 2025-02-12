2437f806334707e5165694af0a0eb99fd9d457a4
Robson Cruz <22680143+robson-cruz@users.noreply.github.com>
2025-02-11 21:18:05 -0300

    update cites data

--- deploy.R
+++ deploy.R
@@ -2,4 +2,4 @@ rsconnect::deployApp('D:/data/endangeredBrazilianPlantSpecies/app/',
                      appTitle = 'specBra', appName = 'specBra',
                      account = 'cbn4rj-robson-cruz',
                      forceUpdate = TRUE,
-                     appId = 4367290)
+                     appId = 13974904)
